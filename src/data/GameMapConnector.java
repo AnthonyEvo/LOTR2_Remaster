01f6948308baff69080d1d2ce4d6c06269d721e3
Anthony <Anthony@MCV-1>
2022-10-23 23:07:55 +0300

    Map objects patterns added

--- src/data/GameMapConnector.java
+++ src/data/GameMapConnector.java
@@ -1,9 +1,42 @@
 package data;
 
-public class GameMap {
+public class GameMapConnector {
+	
 	/* Карта в виде четырехугольных тайлов с горизонтальным сдвигом.
 	 * Размер карты опциональный. Перемещение расчитывать в 8-ми направлениях
 	 * Карты 2-х типов, созданные и сгенерированные. 
 	 * 
 	 * */
+	
+	GameMapConnector() {
+	
+	}
+}
+
+class GlobalMap {
+	
+}
+
+class GlobalMapTile {
+	
+}
+
+class LocalMap {
+	
+}
+
+class LocalMapTile {
+	
+}
+
+class Building implements Productionable{
+	
+}
+
+interface Habitable {
+	
+}
+
+interface Productionable {
+	
 }
