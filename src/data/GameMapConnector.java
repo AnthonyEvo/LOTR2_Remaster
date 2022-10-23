041c15eaaf89272b22a0a2d4b3bc7947b7580f32
Hnatyshyn-Volodymyr <81352298+Hnatyshyn-Volodymyr@users.noreply.github.com>
2022-10-24 00:17:23 +0300

    Game map generator

--- src/data/GameMapConnector.java
+++ src/data/GameMapConnector.java
@@ -1,5 +1,7 @@
 package data;
 
+import java.awt.Color;
+
 public class GameMapConnector {
 	
 	/* Карта в виде четырехугольных тайлов с горизонтальным сдвигом.
@@ -8,17 +10,58 @@ public class GameMapConnector {
 	 * 
 	 * */
 	
-	GameMapConnector() {
+	public static void main(String args[]) {
+		new GlobalMap(10, 10);
+	}
 	
+	GameMapConnector() {
+		
 	}
 }
 
 class GlobalMap {
+	final int mapWidth, mapHeight;
 	
+	GlobalMapTile[][] globalMapTileGrid;
+
+	GlobalMap(int width, int height) {
+		mapWidth = width;
+		mapHeight = height;
+		
+		globalMapTileGrid = new GlobalMapTile[mapWidth][mapHeight];
+		
+		fillMap();
+	}
+	
+	private void fillMap() {
+		for(int i = 0, j = 0; j < globalMapTileGrid[0].length; i++) {
+			globalMapTileGrid[i][j] = new GlobalMapTile(GlobalMapTileType.field);
+			
+			System.out.print(globalMapTileGrid[i][j].getTileTag() + "\t");
+			
+			if(i + 1 == globalMapTileGrid.length) {
+				i = -1;
+				j++;
+				System.out.println();
+			}
+		}
+	}
 }
 
 class GlobalMapTile {
+	GlobalMapTileType tileType;
 	
+	GlobalMapTile (GlobalMapTileType tileType) {
+		this.tileType = tileType;
+	}
+	
+	public Color getTileColor() {
+		return tileType.getColor();
+	}
+	
+	public String getTileTag() {
+		return tileType.getTag();
+	}
 }
 
 class LocalMap {
@@ -31,6 +74,33 @@ class LocalMapTile {
 
 class Building implements Productionable{
 	
+}
+
+enum GlobalMapTileType{
+	forest,
+	mountain,
+	field,
+	sea;
+	
+	public Color getColor() {
+		switch(this) {
+		case forest: return Color.green;
+		case mountain: return Color.darkGray;
+		case field: return Color.lightGray;
+		case sea: return Color.cyan;
+		default: return Color.white;
+		}
+	}
+	
+	public String getTag() {
+		switch(this) {
+		case forest: return "[FRT]";
+		case mountain: return "[MTN]";
+		case field: return "[FLD]";
+		case sea: return "[SEA]";
+		default: return "[EPT]";
+		}
+	}
 }
 
 interface Habitable {
