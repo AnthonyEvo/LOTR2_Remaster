8383a8dca85a336fc6a80b721d22558cb24c9f4d
Anthony <Anthony@MCV-1>
2023-03-20 15:58:06 +0300

    Added initial structure of oktree oprational data base

--- src/data/IngameObjectsDataBase.java
+++ src/data/IngameObjectsDataBase.java
@@ -1,5 +1,9 @@
 package data;
 
+import java.util.TreeMap;
+
+import data.units.Vector3;
+
 public class IngameObjectsDataBase {
 	
 	IngameObjectsSetID baseSetID;
@@ -11,12 +15,26 @@ public class IngameObjectsDataBase {
 	public void volumetricSearch() {
 		
 	}
+	
+	public static void main(String args[]) {
+		new OktreeDataBase(3);
+	}
 }
 
 class OktreeDataBase {
-	double layerDepth, sectorMinSize = 10;
 	
-	OktreeDataBase() {
-		
+	TreeMap<OktreeDataCell, Vector3> gameSpace = new TreeMap<OktreeDataCell, Vector3>();
+	
+	OktreeDataCell mainCell;
+	
+	double layerDepth, sectorMinSize = 10;
+	public static int ODBdepth;
+	
+	OktreeDataBase(int dbDepth) {
+		ODBdepth = dbDepth;
+		mainCell = new OktreeDataCell(dbDepth, new Vector3(), sectorMinSize);
 	}
+	
+	
+	
 }
--- src/data/OktreeDataCell.java
+++ src/data/OktreeDataCell.java
@@ -8,14 +8,23 @@ import data.units.Vector3;
 class OktreeDataCell {
 	
 	final int cellLayer;
-	Vector3 cellPosition;
+	final Vector3 cellPosition;
+	
+	double cellSize, minimalCellSize;   
 	
 	ArrayDeque<GameObject> celledGObjects;
 	OktreeDataCell[] celles = new OktreeDataCell[8];
 	
-	OktreeDataCell(int  cellLayer, Vector3 cellPosition) {
+	OktreeDataCell(int cellLayer, Vector3 cellPosition, double minimalCellSize) {
 		this.cellLayer = cellLayer;
 		this.cellPosition = cellPosition;
+		this.minimalCellSize = minimalCellSize;
+		cellSize = minimalCellSize * cellLayer;
+		
+		for(int i = cellLayer; i < OktreeDataBase.ODBdepth; i++ ) { System.out.print(":   "); }
+		System.out.println("x: " + cellPosition.getX() + ";y: " + cellPosition.getY() + ";x: " + cellPosition.getZ() + "; " + cellSize);
+		
+		if(cellLayer > 1) buildCells();
 	}
 	
 	public void addGObjectToCell(GameObject gObject) {
@@ -23,9 +32,24 @@ class OktreeDataCell {
 	}
 	
 	protected void buildCells() {
-		
 		for(int i = 0; i < 8; i++) {
+			double x, y, z;
 			
+			if(i < 4) { 
+				z = cellPosition.getZ() + cellSize / 4;
+				if(i < 2) { x = cellPosition.getX() + cellSize / 4; } 
+				else { x = cellPosition.getX() - cellSize / 4; }
+			} 
+			else { 	
+				z = cellPosition.getZ() - cellSize / 4;
+				if(i < 6) { x = cellPosition.getX() + cellSize / 4; }
+				else { x = cellPosition.getX() - cellSize / 4; }
+			}
+			
+			if(i == 0 || i == 3 || i == 4 || i == 7) { y = cellPosition.getY() + cellSize / 4; }
+			else { y = cellPosition.getY() - cellSize / 4; }
+			
+			celles[i] = new OktreeDataCell(cellLayer - 1, new Vector3(x,y, z), minimalCellSize);
 		}
 	}
 }
